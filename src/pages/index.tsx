7d95db342831b2aa611934dd7073aababa822d94
macias34 <propiesek@gmail.com>
2023-09-05 20:01:37 +0200

    upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <main className=" min-h-screen bg-foreground">
+      <Button>Kup kursa</Button>
+    </main>
+  );
+}
