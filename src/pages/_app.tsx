38a75ee6cb745d1b701e2f797df25422fdd6461b
macias34 <propiesek@gmail.com>
2023-09-23 13:49:50 +0200

    Added Nunito font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,18 @@ import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { Nunito } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { Toaster } from "@/shared/components/toaster";
 import "@/styles/globals.css";
+
+const nunito = Nunito({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const queryClient = new QueryClient();
 
@@ -14,13 +22,24 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session | null }>) {
   return (
-    <SessionProvider session={session}>
-      <QueryClientProvider client={queryClient}>
-        <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
-          <Component {...pageProps} />
-          <Toaster />
-        </NextThemesProvider>
-      </QueryClientProvider>
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${nunito.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+          >
+            <Component {...pageProps} />
+            <Toaster />
+          </NextThemesProvider>
+        </QueryClientProvider>
+      </SessionProvider>
+    </>
   );
 }
