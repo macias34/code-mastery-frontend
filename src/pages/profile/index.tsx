d57531dc68aabe7d9dec67ffd785ad8d18833411
creend <creend42@gmail.com>
2023-09-24 08:58:45 +0200

    Create my courses page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { type GetServerSideProps } from "next";
 
 import { withRoleAuthorization } from "@/features/auth";
 import { ShopLayout } from "@/features/shop";
