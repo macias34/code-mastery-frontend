1127ac6bea4934f1e53f3f645b1ea39359295842
creend <creend42@gmail.com>
2023-09-27 09:32:29 +0200

    changes in profile list page

--- src/features/dashboard/components/manage-users/user.tsx
+++ src/features/dashboard/components/manage-users/user.tsx
@@ -1,15 +1,36 @@
-import { type UserDto } from "@/features/user";
-import { Avatar, AvatarFallback } from "@/shared/components/avatar";
+import { ShieldCheck } from "lucide-react";
+
+import { type UserDto, UserRole } from "@/features/user";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/shared/components/avatar";
 import { Button } from "@/shared/components/button";
+import { FRONTEND_URL } from "@/shared/constants";
+
+import adminIcon from "../../../../../public/admin.svg";
+import userIcon from "../../../../../public/user.svg";
+import workerIcon from "../../../../../public/worker.svg";
 
 interface Props {
   user: UserDto;
 }
 
+const ROLE_ICONS = {
+  [UserRole.ADMIN]: adminIcon.src,
+  [UserRole.WORKER]: workerIcon.src,
+  [UserRole.USER]: userIcon.src,
+};
+
 export const User = ({ user }: Props) => {
   return (
     <div className="flex gap-x-5">
       <Avatar>
+        <AvatarImage
+          className="bg-white"
+          src={`${FRONTEND_URL}${ROLE_ICONS[user.role]}`}
+        />
         <AvatarFallback>
           {user.username.slice(0, 2).toUpperCase()}
         </AvatarFallback>
